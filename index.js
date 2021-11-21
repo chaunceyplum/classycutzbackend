d2f8dd57de5b7ee8bb5294be2261d89eee66f242
Chauncey <Chaunceyplum@gmail.com>
2021-11-21 13:56:15 -0500

    GETTING READY FOR DEPLOYMENT

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import Express from "express"
 import cutsRouter from './cutsRouter.js'
 import dotenv from 'dotenv'
- 
+import cors from 'cors'
 const app = Express()
 dotenv.config()
 const port = 3000
 app.use(Express.json())
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors({
+    origin:'*'
+    
+}))
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 app.use('/', cutsRouter)
 
