754654a26becffe19d03889ec2572accb31f9b11
Chauncey <Chaunceyplum@gmail.com>
2021-11-21 13:33:06 -0500

    GETTING READY FOR DEPLOYMENT

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import Express from "express"
 import cutsRouter from './cutsRouter.js'
-
-
+import dotenv from 'dotenv'
+ 
 const app = Express()
+dotenv.config()
 const port = 3000
 app.use(Express.json())
 
+
 app.use('/', cutsRouter)
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(process.env.PORT || port, () => console.log(`listening on port ${port}`))
 
 export default app
