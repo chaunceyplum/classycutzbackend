0709496aa89e37edc9c71ce56232f639ac08e98c
Chauncey <Chaunceyplum@gmail.com>
2022-01-02 17:01:48 -0500

    added invisr route for interview

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 import Express from "express"
 import cutsRouter from './cutsRouter.js'
+import invisrRouter from './invisrRouter.js'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
 const app = Express()
 dotenv.config()
+
 const port = 3000
+
 app.use(Express.json())
+app.use(morgan('tiny'));
+
 app.use(cors({
-    origin:'*'
-    
+    origin:'*'   
 }))
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
 
 app.use('/', cutsRouter)
+app.use('/invisr', invisrRouter)
+
+// const uri = process.env.URI
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+// const connection = mongoose.connection
+// connection.once('open', () => {
+//     console.log("MongoDB database connection established sucessfully")
+// })
 
 app.listen(process.env.PORT || port, () => console.log(`listening on port ${port}`))
 
--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,7 @@
-import MongoClient from 'mongodb'
-import assert from 'assert'
+// import MongoClient from 'mongodb'
+const MongoClient = require('mongodb').MongoClient
+// import assert from 'assert'
+const assert = require('assert')
 
 const url= 'mongodb://localhost:27017'
 const dbname = 'classycutz'
@@ -11,5 +13,27 @@ MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
 
     const db = client.db(dbname)
 
-    db.createCollection('Users')
+    // db.createCollection('Users', (err, res) => {
+    //     // assert.strictEqual(err, null)
+    //     console.log('Added Collection', res)
+
+    //     const collection = db.collection('Users')
+
+    //     collection.insertOne({
+    //         firstName : 'john',
+    //         lastName : 'doe',
+    //         email: "fakeEmail@gmail.com",
+    //         password: 'Password'
+    //     }, (err, res) => {
+    //         // assert.strictEqual(err, null)
+    //         console.log('Insert Document:', res.ops)
+
+    //         collection.find().toArray((err,docs) => {
+    //             // assert.strictEqual(err,null)
+    //             console.log('Found Documents: ', docs)
+
+    //             client.close()
+    //         })
+    //     })
+    // })
 })
