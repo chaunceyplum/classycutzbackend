40e350134686fc9909bc7fad528f63af90e18750
Chauncey <Chaunceyplum@gmail.com>
2022-01-03 03:00:24 -0500

    added invisr route for interview

--- invisr.js
+++ invisr.js
@@ -1,12 +1,9 @@
-const invisr = [
+const invisr = 
     {
         "title":"What is find a lobby",
         "description":" Find a lobby is an application that gamers can use to find other gamers to play with. Gamers can search by game or search by console to see which gamers most fit need their needs. Gamers also have the option of making their own lobby where other gamers looking to play the same game can connect."
-    },
-    {
-
     }
-]
+
 
 
 export default invisr
