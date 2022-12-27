51cb74af43a83904b2e4ea19aa7d53903c0b6dd1
Chauncey <Chaunceyplum@gmail.com>
2022-12-27 13:54:09 -0500

    added authentication

--- signUpRouter.js
+++ signUpRouter.js
@@ -1,42 +1,54 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import { userSchema, Users, connect, URI } from './mongoo.js'
+import bcrypt from 'bcryptjs'
 
 const signUpRouter = express.Router()
 
+const saltRounds = 10
+
 signUpRouter.route('/').post(async (req, res) => {
-  const user = new Users({
-    _id: mongoose.Types.ObjectId(),
-    email: req.body.email,
-    pass: req.body.password,
-    name: req.body.name,
-  })
-  const userEmail = { email: req.body.email }
-  const emailSearch = Users.findOne({ userEmail }).exec()
-  if (!emailSearch) {
-    res.json({
-      status: 'FAILED',
-      userFound: true,
-      message: "Someone owns an account with the email you've entered",
+  try {
+    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds)
+    const insertResult = await Users.create({
+      _id: mongoose.Types.ObjectId(),
+      email: req.body.email,
+      password: hashedPwd,
+      name: req.body.name,
     })
-  } else {
-    await user.save().then(
-      res.json({
-        status: 'OK',
-        userFound: false,
-        message: 'Account Created!',
-      })
-    )
+    console.log(`New User Added ${insertResult.email}`)
+    res.send(insertResult)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: 'Username Not Available' })
   }
-  //
-  // !Users.findOne({ userEmail }).exec()
-  // !user
-  //   ? res.json({
-  //       status: 'FAILED',
-  //       userFound: true,
-  //       message: "Someone owns an account with the email you've entered",
-  //     })
-  //   : await user.save()
 })
+
+// signUpRouter.route('/').post(async (req, res) => {
+//   const user = new Users({
+//     _id: mongoose.Types.ObjectId(),
+//     email: req.body.email,
+//     pass: req.body.password,
+//     name: req.body.name,
+//   })
+//   const userEmail = { email: req.body.email }
+//   const emailSearch = Users.findOne({ userEmail }).exec()
+//   if (!emailSearch) {
+//     res.json({
+//       status: 'FAILED',
+//       userFound: true,
+//       message: "Someone owns an account with the email you've entered",
+//     })
+//   } else {
+//     await user.save().then(
+//       res.json({
+//         status: 'OK',
+//         userFound: false,
+//         message: 'Account Created!',
+//       })
+//     )
+//   }
+
+// })
 
 export default signUpRouter
--- userRouter.js
+++ userRouter.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import mongoose, { createConnection, Schema } from 'mongoose'
 import { userSchema, Users, connect, URI } from './mongoo.js'
-
+import bcrypt from 'bcryptjs'
 const userRouter = express.Router()
 
 //routing
-
+const saltRounds = 10
 //sends a list of all users
 userRouter.route('/').get((req, res, next) => {
   // Users.find({}, (data, err) => {
@@ -17,24 +17,48 @@ userRouter.route('/').get((req, res, next) => {
   res.send(data)
 })
 // checks if User is in database
-userRouter.route('/').post((req, res) => {
-  let user = Users.findOne({
-    email: req.body.email,
-    password: req.body.password,
-  }).exec()
-
-  user
-    ? res.json({
-        status: 'OK',
-        userFound: 'true',
-        message: ' Found User!',
-      })
-    : res.json({
-        status: 'BAD',
-        userFound: false,
-        message: ' Cannot find User!',
-      })
+userRouter.route('/').post(async (req, res) => {
+  try {
+    const user = await Users.findOne({ email: req.body.email })
+    console.log(user)
+    if (user) {
+      const cmp = await bcrypt.compare(req.body.password, user.password)
+      if (cmp) {
+        //   ..... further code to maintain authentication like jwt or sessions
+        res.json({
+          auth: 'OK',
+          email: `${req.body.email}`,
+          name: `${req.body.name}`,
+        })
+      } else {
+        res.send('Wrong username or password.')
+      }
+    } else {
+      res.send('Wrong username or password.')
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Internal Server error Occured')
+  }
 })
+// userRouter.route('/').post((req, res) => {
+//   let user = Users.findOne({
+//     email: req.body.email,
+//     password: req.body.password,
+//   }).exec()
+
+//   user
+//     ? res.json({
+//         status: 'OK',
+//         userFound: 'true',
+//         message: ' Found User!',
+//       })
+//     : res.json({
+//         status: 'BAD',
+//         userFound: false,
+//         message: ' Cannot find User!',
+//       })
+// })
 //updates fields
 userRouter.route('/').put((req, res) => {
   let update = {
